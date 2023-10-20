45bc2da9912e8742d69f152c368afe0b162f0f6d
James Tan <jamestansx@gmail.com>
2023-10-20 17:24:39 +0800

    feat: Set catppuccin as default colorscheme

--- lua/jamestansx/init.lua
+++ lua/jamestansx/init.lua
@@ -1,10 +1,5 @@
 -- Global variables
 vim.g.istty = os.getenv("DISPLAY") == nil and os.getenv("WAYLAND_DISPLAY") == nil
 
+-- Configurations
 require("jamestansx.lazy")
-
-if vim.g.istty then
-	vim.cmd.colorscheme("industry")
-else
-	vim.cmd.colorscheme("catppuccin")
-end
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,8 +3,10 @@ vim.g.catppuccin_flavour = "mocha"
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	lazy = true,
 	priority = 1000,
+	init = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 	opts = {
 		transparent_background = true,
 		show_end_of_buffer = true,
