d009cf4a7938556c38f26b18b62f04952ec3ee51
James Tan <jamestansx@gmail.com>
2023-10-21 22:49:45 +0800

    chore: Add code style configs

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,14 @@
 vim.g.catppuccin_flavour = "mocha"
 
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	init = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
-	opts = {
-		transparent_background = true,
-		show_end_of_buffer = true,
-	},
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    init = function()
+        vim.cmd.colorscheme("catppuccin")
+    end,
+    opts = {
+        transparent_background = true,
+        show_end_of_buffer = true,
+    },
 }
